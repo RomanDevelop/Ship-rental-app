a7d3f8e163676a016397a2356cfd37805efc37cb
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-07-11 07:44:57 +0200

    bloc and bd connected

--- lib/data/models/ship.dart
+++ lib/data/models/ship.dart
@@ -4,17 +4,19 @@ class Ship {
   final double fuelCapacity;
   final double pricePerHour;
 
-  Ship(
-      {required this.model,
-      required this.distance,
-      required this.fuelCapacity,
-      required this.pricePerHour});
+  Ship({
+    required this.model,
+    required this.distance,
+    required this.fuelCapacity,
+    required this.pricePerHour,
+  });
 
   factory Ship.fromMap(Map<String, dynamic> map) {
     return Ship(
-        model: map['model'],
-        distance: map['distance'],
-        fuelCapacity: map['fuelCapacity'],
-        pricePerHour: map['pricePerHour']);
+      model: map['model'],
+      distance: (map['distance'] as num).toDouble(),
+      fuelCapacity: (map['fuelCapacity'] as num).toDouble(),
+      pricePerHour: (map['pricePerHour'] as num).toDouble(),
+    );
   }
 }
