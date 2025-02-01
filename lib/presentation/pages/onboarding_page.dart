41a463e4f20d5e4ac2722c0c67ce0c2666048d59
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2025-02-01 17:56:45 +0100

    singlechildscrollview widget added

--- lib/presentation/pages/onboarding_page.dart
+++ lib/presentation/pages/onboarding_page.dart
@@ -18,73 +18,75 @@ class OnboardingPage extends StatelessWidget {
             ],
           ),
         ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 170,
-            ),
-            const Text(
-              'AMASUS',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 240, // Adjust this value to control the image height
-              child: Container(
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/onboarding.png'),
-                        fit: BoxFit.contain)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 170,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Premium ships \nEnjoy the luxury',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Amasus is able to select the right \nvessel for every cargo and route.',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 320,
-                    height: 54,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pushAndRemoveUntil(
-                              MaterialPageRoute(
-                                  builder: (context) => ShipListScreen()),
-                              (route) => false);
-                        },
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.black,
-                            backgroundColor: Colors.white),
-                        child: const Text(
-                          'Drop Cargo',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        )),
-                  ),
-                  const SizedBox(height: 20), // Adding a bit of bottom spacing
-                ],
+              const Text(
+                'AMASUS',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
               ),
-            )
-          ],
+              SizedBox(
+                height: 240, // Adjust this value to control the image height
+                child: Container(
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/onboarding.png'),
+                          fit: BoxFit.contain)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Premium ships \nEnjoy the luxury',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Amasus is able to select the right \nvessel for every cargo and route.',
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: 320,
+                      height: 54,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) => ShipListScreen()),
+                                (route) => false);
+                          },
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: Colors.white),
+                          child: const Text(
+                            'Drop Cargo',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                    const SizedBox(height: 20), // Adding a bit of bottom spacing
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
